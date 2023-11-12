36f9c5f20698b78a29d9bcd1a4e60654a2c7895b
ldy <ldy@qq.com>
2023-11-12 18:50:39 +0800

    hot-fix commit

--- src/main/java/com/xytk/git.java
+++ src/main/java/com/xytk/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello git1");
         System.out.println("hello git11");
         System.out.println("hot-fix git11");
+        System.out.println("push git11");
     }
 }
